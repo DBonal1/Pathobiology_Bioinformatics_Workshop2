18d5ebfcf734fdf8ae7175304c19a6fd54ca7648
DBonal1 <128184102+DBonal1@users.noreply.github.com>
2023-03-20 14:16:40 -0400

    Update 13_Salmon_Index_Creation.sh

--- 13_Salmon_Index_Creation.sh
+++ 13_Salmon_Index_Creation.sh
@@ -7,43 +7,33 @@
 #SBATCH --mem=32G
 # (OPTIONAL)
 # Specifies location for all slurms to be written
-#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_Create_Directories_%j.out
+#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_13_SALMON_Index_Creation_%j.out
 
 #Change directory to salmon index file
-cd /users/USERNAME/data/genome/Salmon_Index_Hs_GRCh38
-
- 
-
+cd /users/USERNAME/scratch/Workshop2/genome/Salmon_Index_Hs_GRCh38
 
 # Load modules
-
-module load  salmon/1.3.0   #### most recent version on oscar
+module load salmon/1.3.0   #### most recent version on oscar
 module load boost/1.69
-module load  gcc/8.3
+module load gcc/8.3
 
-#Unzip your combined genome/transcriptome file 
-gunzip Homo_sapiens_GRCh38_gentrome.fa.gz  
+##### Create variable of of genome/transcriptome file that is unzipped
+gentrome=Homo_sapiens_GRCh38_gentrome.fa.gz
 
-##### create variable of of genome/transcriptome file that is unzipped
-gentrome=Homo_sapiens_GRCh38_gentrome.fa 
-
-decoy=/users/USERNAME/data/genome/Salmon_Index_Hs_GRCh38/decoys.txt 
+##Create Variable for the decoy file
+decoy=decoys.txt
 
 # Define the output directory for the Salmon index
-output_dir=/users/USERNAME/data/genome/Salmon_Index_Hs_GRCh38
+output_dir=/users/USERNAME/scratch/Workshop2/genome/Salmon_Index_Hs_GRCh38
 
 # Create the Salmon index
-salmon index -t $gentrome -d $decoy -p 8 -i $output_dir --gencode 
+salmon index -t $gentrome -d $decoy -p 12 -i $output_dir --gencode
+salmon index 
 
-##### -t  transcript/genome file
-##### -d  decoy file
-##### -p  number of threads used
-##### -i   name of the index that Salmon will create
-##### --gencode  ndicates that the genome fasta file is in Gencode format
-###### -g   annotation file
+echo "Great Job!!!"
 
-#################    save space and rezip your files
-gzip gentrome   
+
+ 
 
 
 echo "Great Job!!!"    ##### Random Comment Added
