91f1a9a1ef3b18efed33e7df41daeebbf5e146f0
DBonal1 <128184102+DBonal1@users.noreply.github.com>
2023-03-20 12:55:32 -0400

    Update 12_Decoy_TXT_Creation.sh

--- 12_Decoy_TXT_Creation.sh
+++ 12_Decoy_TXT_Creation.sh
@@ -5,6 +5,9 @@
 #SBATCH -t 12:00:00
 # Specifies memory to allocate
 #SBATCH --mem=32G
+# (OPTIONAL)
+# Specifies location for all slurms to be written
+#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_Create_Directories_%j.out
 
 
 # Define the paths to the genome assembly fasta file  in zipped format
