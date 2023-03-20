5e5c4f627e286514b3a548a9a89a636121b3ab93
DBonal1 <128184102+DBonal1@users.noreply.github.com>
2023-03-20 12:47:47 -0400

    Update 2_My_first_batchscript.sh

--- 2_My_first_batchscript.sh
+++ 2_My_first_batchscript.sh
@@ -5,6 +5,9 @@
 #SBATCH -t 12:00:00
 # Specifies memory to allocate
 #SBATCH --mem=32G
+# (OPTIONAL)
+# Specifies location for all slurms to be written
+#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_My_first_batchscript_%j.out
 
 
 echo "I did it!!!"
