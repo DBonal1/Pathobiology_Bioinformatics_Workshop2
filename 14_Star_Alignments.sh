2b9d2d40d240a59524fcf17b20c49505ea76ea9d
DBonal1 <128184102+DBonal1@users.noreply.github.com>
2023-03-20 12:56:02 -0400

    Update 14_Star_Alignments.sh

--- 14_Star_Alignments.sh
+++ 14_Star_Alignments.sh
@@ -5,6 +5,9 @@
 #SBATCH -t 12:00:00
 # Specifies memory to allocate
 #SBATCH --mem=32G
+# (OPTIONAL)
+# Specifies location for all slurms to be written
+#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_Create_Directories_%j.out
 
 # Load the STAR module
 module load  star/2.6.1b   #### most recent version on oscar
