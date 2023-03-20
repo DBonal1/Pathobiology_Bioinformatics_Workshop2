a70fb64ed7f425fe4d9d65b2987edd7a9d1bc00b
DBonal1 <128184102+DBonal1@users.noreply.github.com>
2023-03-20 12:54:06 -0400

    Update 7_Skewer_Batchscript.sh

--- 7_Skewer_Batchscript.sh
+++ 7_Skewer_Batchscript.sh
@@ -5,6 +5,9 @@
 #SBATCH -t 24:00:00
 # Specifies memory to allocate
 #SBATCH --mem=32G
+# (OPTIONAL)
+# Specifies location for all slurms to be written
+#SBATCH -o /users/USERNAME/scratch/Workshop2/slurms/SLURM_Create_Directories_%j.out
 
 ## Load modules required for script commands,  So this loads the most recent version of FastQC.  
 module load skewer/0.2.1
